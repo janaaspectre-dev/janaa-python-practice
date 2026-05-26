6f518be923bb5329c4e4071585ba0dfd3d799703
janaaspectre-dev <janaaspectre@gmail.com>
2026-05-26 14:23:22 +0530

    Add files via upload

---
+++ DAY - 9 (PROBS).py
@@ -0,0 +1,89 @@
+#PROBLEMS:
+
+
+"""#1)Create a list with 5 numbers and print it
+lst = [1,2,3,4,5]
+print(lst)
+
+
+
+#2)Add a new element to a list using append().
+lst = [1,2,3,4,5]
+lst.append(6,7,8)
+print(lst)
+
+
+
+#3)Insert an element at index 2.
+lst = ["USA","UK","USSR"]
+lst.insert(2,"FRANCE")
+print(lst)
+
+
+
+#4)Remove an element using remove().
+lst = ['USA', 'UK', 'FRANCE', 'USSR', 'NAZI GERMANY']
+lst.remove("NAZI GERMANY")
+print(lst)
+
+
+
+#5)Remove last element using pop().
+lst = ['USA', 'UK', 'FRANCE', 'USSR']
+lst.pop()
+print(lst)
+
+
+
+#6)Find the length of a list
+lst = ['USA', 'UK', 'FRANCE', 'USSR']
+x =len(list(lst))
+print(x)
+
+
+#7)Access first and last element.
+lst = list(input())
+lst.insert(1,)
+
+
+
+#8)find maximum and minimum numbers.
+lst = [1,2,3,4,5]
+y=max(lst)
+x=min(lst)
+print("max=",y)
+print("min=",x)"""
+
+
+
+#9)Find sum of all elements.
+lst = [1,2,3,4,5]
+x = sum(lst)
+print(lst)
+print(x)
+
+
+
+
+#10)Count how many times a number appears.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
